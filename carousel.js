9357ad43fc057ab1c024cf0282bb70b339e382fd
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-03-20 12:49:19 -0500

    first commit

---
+++ carousel.js
@@ -0,0 +1,35 @@
+function showSelectedSlide(id) {
+    console.log(id);
+}
+
+/*var slideIndex = 1;
+showSlide(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlide(n) {
+    
+    
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("slide-adv-btn");
+    
+    
+
+  if (n > slides.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = slides.length;}
+  /*for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active-dot";
+}*/
