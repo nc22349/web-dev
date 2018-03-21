636afbcb1658edd38c4d6d179fb9d71649d401a9
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-03-21 13:17:14 -0500

    Got preliminary slide JS working

--- carousel.js
+++ carousel.js
@@ -1,35 +1,22 @@
-function showSelectedSlide(id) {
-    console.log(id);
+var currentSlideIndex = 0;
+
+function showSelectedSlide(slideIndex) {
+    if (slideIndex != currentSlideIndex) {
+        currentSlideIndex = slideIndex;
+        changeToSlide(slideIndex);
+    }
 }
 
-/*var slideIndex = 1;
-showSlide(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlide(n) {
-    
-    
-    var i;
+function changeToSlide(index) {
     var slides = document.getElementsByClassName("slide");
+    for (var i=0; i<slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[index].style.display = "block";
+    
     var dots = document.getElementsByClassName("slide-adv-btn");
-    
-    
-
-  if (n > slides.length) {slideIndex = 1;}
-  if (n < 1) {slideIndex = slides.length;}
-  /*for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
+    for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active-dot", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active-dot";
-}*/
+    }
+    dots[index].className += " active-dot";
+}
