ccebc298896698af462e4ff15be691cc1958774c
Zee Hussain <zee_hussain@icloud.com>
2018-04-08 13:51:34 -0500

    adding Become A Photographer page

---
+++ Project_Phase_IV/Become_A_Photographer/index.php
@@ -0,0 +1,45 @@
+<!-- CONTACT US index.html -->
+
+<html>
+    <head>
+        <title>Contact Us</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="../global.css">
+    </head>
+    <body>
+        <div class="container setmax">
+            <div class="py-5 text-center">
+                <h2>Become a Photographer</h2>
+                <p class="lead">Good with a camera? Looking for clients? Join our community of photographers to take potraits of future UT Grads!</p>
+            </div>
+            <form class="needs-validation" novalidate>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="firstName">First name</label>
+                        <input type="text" class="form-control" id="firstName" placeholder value required>
+                        <div class="invalid-feedback">First name is required.</div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="lastName">Last name</label>
+                        <input type="text" class="form-control" id="lastName" placeholder value required>
+                        <div class="invalid-feedback">Last name is required.</div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                    <div class="invalid-feedback">Valid email is required.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="***********">
+                    <div class="invalid-feedback">Your Password Must Contain: <table><tr><th>One Capital Letter</th><th>One Special Character</th><th>One Number</th></tr></table></div>
+                </div>
+                <hr class="mb-4">
+                <button class="btn btn-lg burnt-orange" type="submit">Register</button>
+            </form>
+        </div>ftyrtdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    </body>
+</html>
