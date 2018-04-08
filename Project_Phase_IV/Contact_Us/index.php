23a8bdd73909cab77c07a749dd6f18a339aa5e54
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-04-07 21:32:46 -0500

    Made footer a little prettier

--- Project_Phase_IV/Contact_Us/index.php
+++ Project_Phase_IV/Contact_Us/index.php
@@ -3,10 +3,7 @@
 <html>
     <head>
         <title>Contact Us</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="../global.css">
+	<?php include "../head-content.html"; ?>
     </head>
     <body>
         <div class="container setmax">
