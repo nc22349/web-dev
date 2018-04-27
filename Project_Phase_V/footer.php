2a4a6068f9d97012b01868a688a9acbdb42ed500
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-04-27 15:03:38 -0500

    Finished adapting footer

--- Project_Phase_V/footer.php
+++ Project_Phase_V/footer.php
@@ -6,22 +6,22 @@
     }
 
     print <<<FOOTER
-        <footer class="burnt-orange container-fluid">
+        <footer class="burnt-orange container-fluid pt-3 pb-1">
             <div class="row">
-                <div class="col-4 col-md text-center">
-                    <a href="$cd"><img src="${cd}Global_Images/Logo.png" alt="Home" style="height: 60px;"</img></a>
+                <div class="col-12 col-md text-center">
+                    <a href="$cd"><img src="${cd}Global_Images/Logo.png" alt="Home" style="height: 70px;"</img></a>
                 </div>
-                <div class="col-4 col-md">
-                    <ul class="list-unstyled text-small">
-                        <li><a class="text-burnt-orange" href="${cd}Become_A_Photographer">Become a Photographer</a></li>
-                        <li><a class="text-burnt-orange" href="${cd}About/about.html">About the Developers</a></li>
-                        <li><a class="text-burnt-orange" href="${cd}Help">Help</a></li>
+                <div class="col-6 col-md">
+                    <ul class="list-unstyled text-small text-center">
+                        <li><a href="${cd}Become_A_Photographer">Become a Photographer</a></li>
+                        <li><a href="${cd}About/about.html">About the Developers</a></li>
+                        <li><a href="${cd}Help">Help</a></li>
                     </ul>
                 </div>
-                <div class="col-4 col-md">
-                    <ul class="list-unstyled text-small">
-                        <li><a class="text-burnt-orange" href="${cd}FAQ">FAQ</a></li>
-                        <a class="text-burnt-orange" href="${cd}Site_Feedback">Site Feedback</a></li>
+                <div class="col-6 col-md">
+                    <ul class="list-unstyled text-small text-center">
+                        <li><a href="${cd}FAQ">FAQ</a></li>
+                        <li><a href="${cd}Site_Feedback">Site Feedback</a></li>
                     </ul>
                 </div>
             </div>
