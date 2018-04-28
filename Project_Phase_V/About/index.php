83cd81f9283cfea6a98b4ae679be7ecf6a7e5b8c
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-04-27 21:16:02 -0500

    Small adjustments

--- Project_Phase_V/About/index.php
+++ Project_Phase_V/About/index.php
@@ -10,19 +10,19 @@
 			<div class="py-5 text-center">
 				<h2>About the Developers</h2>
 			</div>
-			<div class="row">
-				<div class="col-lg-6">
+			<div class="row pb-5">
+				<div class="col-md-6">
 					<div class="text-center">
-                        <img class="rounded" src="Nathan.jpg" alt="Nathan" style="max-width: 350px">
+                        <img class="rounded mb-2" src="Nathan.jpg" alt="Nathan" style="max-width: 350px">
                     </div>
-                    <p class="lead">I am a graduating senior in English with professional experience in technical writing and Javascript. In my free time, I play piano, listen to music, and hang out with friends. Favorite TV shows: The Wire, Parks &amp; Rec, Deadwood</p>
+                    <p class="lead">I am a graduating senior in English with professional experience in technical writing and Javascript. In my free time, I play piano, listen to music, and hang out with friends.</p>
                     <p>Email: <a href="mailto:nathan.caldwell@utexas.edu">nathan.caldwell@utexas.edu</a></p>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-md-6">
 					<div class="text-center">
-                        <img class="rounded" src="Zee.jpeg" alt="Zee" style="max-width: 350px">
+                        <img class="rounded mb-2" src="Zee.jpeg" alt="Zee" style="max-width: 350px">
                     </div>
-                    <p class="lead">I am a UT Austin Junior majoring in Economics, with a minor in Computer Science. I also have a passion for photography, hiking/rock climbing, and binge watching The Office</p>
+                    <p class="lead">I am a UT Austin Junior majoring in Economics, with a minor in Computer Science. I also have a passion for photography, hiking/rock climbing, and binge watching The Office.</p>
                     <p>Email: <a href="mailto:zee_hussain@utexas.edu">zee_hussain@utexas.edu</a></p>
                     <p>Phone: 281-323-3604</p>
                 </div>
