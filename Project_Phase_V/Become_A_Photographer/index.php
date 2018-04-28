31a5b098ed2715b63ec2f72c9ba7cd344de3afbe
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-04-27 23:15:50 -0500

    Registration

--- Project_Phase_V/Become_A_Photographer/index.php
+++ Project_Phase_V/Become_A_Photographer/index.php
@@ -1,29 +1,17 @@
 <!-- BECOME A PHOTOGRAPHER index.php -->
 <html>
-<head>
-    <title>Become a Photographer</title>
-    <?php include "../head-content.html"; ?>
-</head>
-<body>
-    <?php include "../navbar.php"; ?>
-    <div class="container setmax">
-        <div class="py-5 text-center">
-            <h2>Become a Photographer</h2>
-            <p class="lead">Good with a camera? Looking for clients? Join our community of photographers to take potraits of future UT Grads!</p>
-        </div>
-
-<?php
-$value = 1;
-if(isset($_COOKIE["success"]))
-{
-    header("Location: ");
-}
-
-else
-{
-    $script = $_SERVER['PHP_SELF'];
-    print <<<REGISTER
-    <form class="needs-validation" id="login" method="post" action=$script>
+    <head>
+        <title>Become a Photographer</title>
+        <?php include "../head-content.html"; ?>
+    </head>
+    <body>
+        <?php include "../navbar.php"; ?>
+        <div class="container setmax">
+            <div class="py-5 text-center">
+                <h2>Become a Photographer</h2>
+                <p class="lead">Good with a camera? Looking for clients? Join our community of photographers to take potraits of future UT Grads!</p>
+            </div>
+            <form class="needs-validation" id="login" method="post" action="register.php">
                 <div class="row">
                     <div class="col-md-6 mb-3">
                         <label for="firstName">First name</label>
@@ -47,45 +35,11 @@ else
                     <div class="invalid-feedback">Your Password Must Contain: <table><tr><th>One Capital Letter</th><th>One Special Character</th><th>One Number</th></tr></table></div>
                 </div>
                 <hr class="mb-4">
-		<div class="text-center">
+                <div class="text-center">
                     <button class="btn btn-lg burnt-orange" name="signup" type="submit">Register</button>
-		</div>
+                </div>
             </form>
-    </div>
-REGISTER;
-
-}
-
-if (isset($_POST["signup"])) 
-{
-    $file = fopen("/user_pass.txt", "r");
-    $user = $_POST['email'];
-    $taken = false;
-    while(!feof($file))
-    {
-        $line = trim(fgets($file));
-        $array = explode(':',$line);
-        if ($array[0] === "$user")
-        {
-            $taken = true;
-            break;
-        }
-    }
-    if ($taken === true)
-    {
-        echo 'alert("The email $user is already associated with an account.")';
-    }
-    else
-    {
-        $password = $_POST['password'];
-        $str = "$user:$password";
-        $file = fopen("./user_pass.txt", "a");
-        fwrite($file, "$str \n");
-        setcookie("success", $value, time() + 86400*1095, "/");
-        header("Location:  ");
-    }
-}
-?>
-<?php include "../footer.php"; ?>
-</body>
+        </div>
+        <?php include "../footer.php"; ?>
+    </body>
 </html>
