215c0b8bfd6faff7a7979063593cb8c0525685e9
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-04-30 15:27:47 -0500

    Create Project Phase VI directory

---
+++ Project_Phase_VI/About/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About the Developers</title>
+		<?php include "../head-content.html" ?>
+	</head>
+	<body>
+	    <?php include "../navbar.php" ?>
+		<div class="container setmax-900">
+			<div class="py-5 text-center">
+				<h2>About the Developers</h2>
+			</div>
+			<div class="row pb-5">
+				<div class="col-md-6">
+					<div class="text-center">
+                        <img class="rounded mb-2" src="Nathan.jpg" alt="Nathan" style="max-width: 350px">
+                    </div>
+                    <p class="lead">I am a graduating senior in English with professional experience in technical writing and Javascript. In my free time, I play piano, listen to music, and hang out with friends.</p>
+                    <p>Email: <a href="mailto:nathan.caldwell@utexas.edu">nathan.caldwell@utexas.edu</a></p>
+                </div>
+                <div class="col-md-6">
+					<div class="text-center">
+                        <img class="rounded mb-2" src="Zee.jpeg" alt="Zee" style="max-width: 350px">
+                    </div>
+                    <p class="lead">I am a UT Austin Junior majoring in Economics, with a minor in Computer Science. I also have a passion for photography, hiking/rock climbing, and binge watching The Office.</p>
+                    <p>Email: <a href="mailto:zee_hussain@utexas.edu">zee_hussain@utexas.edu</a></p>
+                    <p>Phone: 281-323-3604</p>
+                </div>
+            </div>
+        </div>
+		<?php include "../footer.php" ?>
+    </body>
+</html>
