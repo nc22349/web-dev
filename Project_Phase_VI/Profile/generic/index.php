10413c63bf5f0309f895d858ffd2b97344bb985e
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-05-10 15:59:53 -0500

    Hook generic profile to header, footer

--- Project_Phase_VI/Profile/generic/index.php
+++ Project_Phase_VI/Profile/generic/index.php
@@ -1,9 +1,11 @@
+<?php $profile = true; ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>generic profile</title>
-		<?php include "../../head-content.html" ?>
-        <link rel="stylesheet" href="../../global.css">
+	    <title>generic profile</title>
+	    <?php include "../../head-content.html" ?>
+	    <link rel="stylesheet" href="../../global.css">
 	</head>
 	<body>
 	    <?php include "../../navbar.php" ?>
@@ -12,8 +14,11 @@
                 <img class="w-lg-100 mx-auto" src="profile.jpg" alt="profile picture">
             </div>
             <div class="col-lg-6 order-lg-1">
-                <div class="py-5 p-3 text-center">
-                    <h5>David Bowie</h5>
+                <div class="py-5 p-3">
+                    <h5 class="text-center">David Bowie</h5>
+		    <table>
+			<tr>
+			    <td><strong>Bio:</strong></td>
                     <p class="lead"><strong>Bio:</strong>I stepped through the door, floated in a most peculiar way, ended up in Austin, and now I'm taking pictures.</p>
                     <p class="lead"><strong>Languages spoken:</strong>English, Martian</p>
                     <p class="lead"><strong>Price:</strong>$$</p>
