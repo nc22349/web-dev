36e45070056b5f3689bee67fc8ac0c72fe274e75
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-05-10 15:44:29 -0500

    Create generic profile

--- Project_Phase_VI/Profile/generic/index.php
+++ Project_Phase_VI/Profile/generic/index.php
@@ -1,1 +1,28 @@
-sdlfkjlkj
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>generic profile</title>
+		<?php include "../../head-content.html" ?>
+        <link rel="stylesheet" href="../../global.css">
+	</head>
+	<body>
+	    <?php include "../../navbar.php" ?>
+        <div class="row setmax-900">
+            <div class="col-lg-6 order-lg-2 text-center">
+                <img class="w-lg-100 mx-auto" src="profile.jpg" alt="profile picture">
+            </div>
+            <div class="col-lg-6 order-lg-1">
+                <div class="py-5 p-3 text-center">
+                    <h5>David Bowie</h5>
+                    <p class="lead"><strong>Bio:</strong>I stepped through the door, floated in a most peculiar way, ended up in Austin, and now I'm taking pictures.</p>
+                    <p class="lead"><strong>Languages spoken:</strong>English, Martian</p>
+                    <p class="lead"><strong>Price:</strong>$$</p>
+                    <p class="lead"><strong>Payment:</strong>Cash, Paypal</p>
+                    <p class="lead"><strong>Has own transportation:</strong>Yes</p>
+                    <p class="lead"><strong>Rating:</strong>4.5</p>
+                </div>
+            </div>
+        </div>
+		<?php include "../../footer.php" ?>
+    </body>
+</html>
