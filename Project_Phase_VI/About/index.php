6e2bf53db9cdca74b790fac9bda1cbc2344b3cd2
Zee Hussain <zee_hussain@icloud.com>
2018-05-10 18:52:42 -0500

    adding youtube video

--- Project_Phase_VI/About/index.php
+++ Project_Phase_VI/About/index.php
@@ -17,6 +17,12 @@
                     </div>
                     <p class="lead">I am a graduating senior in English with professional experience in technical writing and Javascript. In my free time, I play piano, listen to music, and hang out with friends.</p>
                     <p>Email: <a href="mailto:nathan.caldwell@utexas.edu">nathan.caldwell@utexas.edu</a></p>
+                    <div class="video-wrapper">
+                    <p>Favorite SNL Sketch</p>
+                    <iframe width="350" height="315"
+      				src="https://www.youtube.com/embed/O7VaXlMvAvk?autoplay=1;controls=1">
+      				</iframe>
+      				</div>
                 </div>
                 <div class="col-md-6">
 					<div class="text-center">
@@ -24,7 +30,13 @@
                     </div>
                     <p class="lead">I am a UT Austin Junior majoring in Economics, with a minor in Computer Science. I also have a passion for photography, hiking/rock climbing, and binge watching The Office.</p>
                     <p>Email: <a href="mailto:zee_hussain@utexas.edu">zee_hussain@utexas.edu</a></p>
-                    <p>Phone: 281-323-3604</p>
+                    <p>Favorite SNL Sketch</p>
+                    <div class="video-wrapper">
+                    <p>Favorite SNL Sketch</p>
+                    <iframe width="350" height="315"
+      				src="https://www.youtube.com/embed/q2sI-T69sjs?autoplay=1;controls=1">
+      				</iframe>
+      				</div>
                 </div>
             </div>
         </div>
