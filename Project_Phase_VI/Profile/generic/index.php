a5744c57a377763decf8b640e0ab31e1541f2f8f
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-05-10 16:04:11 -0500

    Rearrange profile info

--- Project_Phase_VI/Profile/generic/index.php
+++ Project_Phase_VI/Profile/generic/index.php
@@ -9,22 +9,39 @@
 	</head>
 	<body>
 	    <?php include "../../navbar.php" ?>
-        <div class="row setmax-900">
-            <div class="col-lg-6 order-lg-2 text-center">
+        <div class="container setmax-900 py-5">
+            <div class="col-lg-6 text-center">
                 <img class="w-lg-100 mx-auto" src="profile.jpg" alt="profile picture">
             </div>
-            <div class="col-lg-6 order-lg-1">
-                <div class="py-5 p-3">
+            <div class="col-lg-6">
+                <div class="p-3">
                     <h5 class="text-center">David Bowie</h5>
-		    <table>
-			<tr>
-			    <td><strong>Bio:</strong></td>
-                    <p class="lead"><strong>Bio:</strong>I stepped through the door, floated in a most peculiar way, ended up in Austin, and now I'm taking pictures.</p>
-                    <p class="lead"><strong>Languages spoken:</strong>English, Martian</p>
-                    <p class="lead"><strong>Price:</strong>$$</p>
-                    <p class="lead"><strong>Payment:</strong>Cash, Paypal</p>
-                    <p class="lead"><strong>Has own transportation:</strong>Yes</p>
-                    <p class="lead"><strong>Rating:</strong>4.5</p>
+                    <table>
+                        <tr>
+                            <td><strong>Bio:</strong></td>
+                            <td>I stepped through the door, floated in a most peculiar way, ended up in Austin, and now I'm taking pictures.</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Languages:</strong></td>
+                            <td>English, Martian</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Price:</strong></td>
+                            <td>$$</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Payment:</strong></td>
+                            <td>Cash, Paypal</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Has own transportation:</strong></td>
+                            <td>Yes</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Rating:</strong></td>
+                            <td>4.5</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
