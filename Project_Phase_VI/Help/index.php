8c1374ba60a2268dc8104d97ddc5f2b414e2a019
Nathan Caldwell <nathan.caldwell@utexas.edu>
2018-05-10 17:16:40 -0500

    Edited existing code for Help page

--- Project_Phase_VI/Help/index.php
+++ Project_Phase_VI/Help/index.php
@@ -2,40 +2,57 @@
 
 <html>
     <head>
-        <title>Contact Us</title>
-	<?php include "../head-content.html"; ?>
+        <title>Help</title>
+        <?php include "../head-content.html"; ?>
     </head>
     <body>
         <?php include "../navbar.php"; ?>
         <div class="container setmax">
             <div class="py-5 text-center">
                 <h2>Contact Us</h2>
-                <p class="lead">Question about the site? Fill out the form below and an ATX Grad Photo representative will get back to you promptly.</p>
+                <p class="lead">Do you need help using the site? Fill out the form below and an ATX Grad Photo representative will get back to you promptly.</p>
             </div>
-            <form class="needs-validation" novalidate>
-                <div class="row">
-                    <div class="col-md-6 mb-3">
-                        <label for="firstName">First name</label>
-                        <input type="text" class="form-control" id="firstName" placeholder value required>
-                        <div class="invalid-feedback">First name is required.</div>
+            <form id="registrationForm" class="needs-validation">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" name="username" id="username" autocomplete="off" required>
+                        <div class="invalid-feedback">That username is taken.</div>
                     </div>
-                    <div class="col-md-6 mb-3">
-                        <label for="lastName">Last name</label>
-                        <input type="text" class="form-control" id="lastName" placeholder value required>
-                        <div class="invalid-feedback">Last name is required.</div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password" id="password" required>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-success">Submit</button>
+                        <button type="reset" class="btn btn-secondary">Reset</button>
+                    </div>
+                </form>
+            <form id="helpForm" class="needs-validation">
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="firstname">First name</label>
+                            <input type="text" class="form-control" name="firstname" id="firstname" required>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="lastname">Last name<span class="text-muted"> (Optional)</span></label>
+                            <input type="text" class="form-control" name="lastname" id="lastname">
+                        </div>
                     </div>
                 </div>
-                <div class="mb-3">
+                <div class="form-group mb-3">
                     <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="you@example.com" required>
                     <div class="invalid-feedback">Valid email is required.</div>
                 </div>
-                <div class="mb-3">
+                <div class="form-group mb-3">
                     <label for="comments">Comments</label>
-                    <textarea class="textarea" id="comments" placeholder="Your comments/question here..."></textarea>
+                    <textarea class="textarea" id="comments" placeholder="Your comments/question here..." required></textarea>
                 </div>
                 <hr class="mb-4">
-                <button class="btn btn-lg burnt-orange" type="submit">Submit</button>
+                <button class="btn btn-lg burnt-orange mx-auto" type="submit">Submit</button>
             </form>
         </div>
         <?php include "./../footer.php"; ?>
